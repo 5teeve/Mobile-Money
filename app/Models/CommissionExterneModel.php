19ef66586bb677fd949c222af1bcf4c4bfa13359
RASOLOFOMAROMALALA Mihaja <rasolo.mihaja@yahoo.com>
2026-07-20 15:27:55 +0300

    feat(models): add model com externe

--- app/Models/CommissionExterneModel.php
+++ app/Models/CommissionExterneModel.php
@@ -18,4 +18,16 @@ class CommissionExterneModel extends Model
 
         return $ligne ? (float) $ligne['taux_pourcentage'] : 0.0;
     }
+
+    protected $validationRules = [
+        'prefixe_id' => 'required|is_natural_no_zero|is_unique[commission_externe.prefixe_id,id,{id}]',
+        'taux_pourcentage' => 'required|numeric|greater_than_equal_to[0]|less_than_equal_to[100]',
+    ];
+
+    public function getAvecPrefixe(): array
+    {
+        return $this->select('commission_externe.*, prefixe.prefixe AS prefixe')
+                    ->join('prefixe', 'prefixe.id = commission_externe.prefixe_id')
+                    ->findAll();
+    }
 }
