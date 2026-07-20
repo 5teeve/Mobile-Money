fca63d84ea34a78d3b59514e2dc794ecc7f93a87
Steeven <steeveric5@gmail.com>
2026-07-20 09:19:15 +0300

    feat(Route): Add Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('client', ['namespace' => 'App\Controllers\Client'], function ($routes) {
+    $routes->get('login', 'AuthController::showLogin');
+    $routes->post('login', 'AuthController::login');
+    $routes->get('logout', 'AuthController::logout');
+
+    $routes->get('dashboard', 'CompteController::index');
+    $routes->get('operations', 'OperationController::index');
+    $routes->post('operations/depot', 'OperationController::depot');
+    $routes->post('operations/retrait', 'OperationController::retrait');
+    $routes->post('operations/transfert', 'OperationController::transfert');
+    $routes->get('historique', 'HistoriqueController::index');
+});
