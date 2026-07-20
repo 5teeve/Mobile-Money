affba52090433a3ea1e7d2f772c58b723bd10905
RASOLOFOMAROMALALA Mihaja <rasolo.mihaja@yahoo.com>
2026-07-20 11:02:55 +0300

    feat(model): Add model

--- app/Models/TypeOperationModel.php
+++ app/Models/TypeOperationModel.php
@@ -11,4 +11,17 @@ class TypeOperationModel extends Model
     protected $returnType    = 'array';
     protected $useTimestamps = false;
     protected $allowedFields = ['code', 'libelle'];
+
+      protected $validationRules = [
+        'code' => 'required|in_list[DEPOT,RETRAIT,TRANSFERT]|is_unique[type_operation.code,id,{id}]',
+        'libelle' => 'required|min_length[2]|max_length[100]',
+    ];
+
+    protected $validationMessages = [
+        'code' => [
+            'in_list' => 'Le code doit être DEPOT, RETRAIT ou TRANSFERT.',
+            'is_unique' => 'Ce code existe déjà.',
+        ],
+    ];
 }
+
